9860efc4c0d2aa0eb1312411f02fade81f8825ed
emas qwaider <148536155+emasqwaider@users.noreply.github.com>
2024-08-03 00:19:09 +0300

    Task7_Time

---
+++ Task7_Time/js/js.js
@@ -0,0 +1,20 @@
+
+const p = document.querySelector('#p');
+
+setInterval(CurrDate, 1000);
+
+function CurrDate() {
+
+    let now = new Date();
+
+    let h = now.getUTCHours()-9;
+    let m = now.getMinutes();
+    let s = now.getSeconds();
+    h = h.toString().padStart(2, '0');
+    m = m.toString().padStart(2, '0');
+    s = s.toString().padStart(2, '0');
+
+    p.innerHTML = h + ":" + m + ":" + s;
+
+
+}
